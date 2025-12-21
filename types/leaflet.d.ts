d66abc054f4a1e3bbf641cea1ae7bb7a76afdc82
Fazetdev <fazetdev@gmail.com>
2025-12-21 22:15:02 +0100

    fix: Update leaflet type declaration for proper module resolution

--- types/leaflet.d.ts
+++ types/leaflet.d.ts
@@ -1,2 +1,8 @@
-declare module 'leaflet';
-declare module 'react-leaflet';
+declare module 'leaflet' {
+  export = L;
+}
+
+declare module 'leaflet/dist/leaflet.css' {
+  const content: string;
+  export default content;
+}
